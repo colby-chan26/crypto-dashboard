9206e5c6768d96e10fdd67d95048415585c40bd2
Colby Chan <colbychan26@gmail.com>
2022-07-01 12:49:24 -0400

    bug fix with search

--- src/App.js
+++ src/App.js
@@ -17,14 +17,14 @@ function App() {
 
 
   useEffect(() => {
-    axios.get('/getInfoCurrency').then(
-    //axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false').then(
+    //axios.get('/getInfoCurrency').then(
+    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false').then(
       res => {
         setCoins(res.data);
       }).catch(error => console.log(error));
 
-    axios.get('/getInfoExchange').then(
-    //axios.get('https://api.coingecko.com/api/v3/exchanges?per_page=250').then(
+    //axios.get('/getInfoExchange').then(
+    axios.get('https://api.coingecko.com/api/v3/exchanges?per_page=250').then(
       res => {
         setExchanges(res.data);
         console.log(res.data)
@@ -32,6 +32,7 @@ function App() {
   }, []);
 
   const handleChange = e => {
+    e.preventDefault();
     setSearch(e.target.value)
   }
 
@@ -58,7 +59,7 @@ function App() {
       {searchMode === 'currency' ? <>
       <div className='coin-search'>
         <h1 className='coin-text'> console.log('Search a currency')</h1>
-        <form>
+        <form onSubmit={(e) => e.preventDefault()}>
           <input type="text" placeholder="Search" className="coin-input" onChange={handleChange} />
         </form>
 
