0f23e7d41b061bb6d67ff95e26fdd6953b9cb183
farriskabalaoui <farris.kabalaoui@gmail.com>
2022-07-01 10:20:37 -0400

    reverted api back to correct mapping.

--- src/App.js
+++ src/App.js
@@ -17,14 +17,14 @@ function App() {
 
 
   useEffect(() => {
-    //axios.get('/getInfoCurrency').then(
-    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false').then(
+    axios.get('/getInfoCurrency').then(
+    //axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false').then(
       res => {
         setCoins(res.data);
       }).catch(error => console.log(error));
 
-    //axios.get('/getInfoExchange').then(
-    axios.get('https://api.coingecko.com/api/v3/exchanges?per_page=250').then(
+    axios.get('/getInfoExchange').then(
+    //axios.get('https://api.coingecko.com/api/v3/exchanges?per_page=250').then(
       res => {
         setExchanges(res.data);
         console.log(res.data)
