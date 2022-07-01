5aa1e5c633d0c4049b3fcd66521404a6352296f6
Colby Chan <colbychan26@gmail.com>
2022-07-01 12:39:08 -0400

    css fix

--- src/components/Exchange.jsx
+++ src/components/Exchange.jsx
@@ -1,23 +1,17 @@
 import React from 'react'
-import './Coin.css';
+import './Exchange.css';
 import '../App.css';
 
 const exchange = ({name, image, country, year_established, trust_score, url}
 ) => {
     return (
-        <div className='coin-container'>
-            <div className='coin-row' >
-                <div className='coin'>
-                    <img src={image} alt="cryptoimage" />
-                    <h1>{name}</h1>
-                </div>
-                <div className='coin-data'>
-                    <p  className='coin-price'> {country}</p>
-                    <p  className='coin-price'>{year_established}</p>
-                    <p  className='coin-price'>{trust_score}/10</p>
-                    <a target="_blank" rel="noopener noreferrer" href={url} className="button4">link</a>
-                </div>
-            </div>
+        <div className='exchange'>
+            <img src={image} alt="cryptoimage" />
+            <h1>{name}</h1>
+            <p  className='exchange-marketcap'> {country}</p>
+            <p  className='exchange-volume'>{year_established}</p>
+            <p  className='exchange-volume'>{trust_score}/10</p>
+            <a target="_blank" rel="noopener noreferrer" href={url} className="button4">link</a>
         </div>
     )
 }
